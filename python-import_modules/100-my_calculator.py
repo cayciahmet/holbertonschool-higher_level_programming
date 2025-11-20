01912e665ef44762f28de4832fd35ee3285ac483
cayciahmet <nbiyevashla215@gmail.com>
2025-11-20 20:00:47 +0000

    6. Build my own calculator!

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,30 +1,24 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
-import sys
+from sys import argv, exit
 
 if __name__ == "__main__":
-    # Arqument sayini yoxlayiriq (proqram adi + 3 arqument = 4)
-    if len(sys.argv) != 4:
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+        exit(1)
 
-    operator = sys.argv[2]
-    ops = ["+", "-", "*", "/"]
+    ops = {
+        "+": add,
+        "-": sub,
+        "*": mul,
+        "/": div
+    }
 
-    # Operatorun duzgunluyunu yoxlayiriq
-    if operator not in ops:
+    if argv[2] not in ops:
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+        exit(1)
 
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
+    a = int(argv[1])
+    b = int(argv[3])
 
-    # Hesablama
-    if operator == "+":
-        print("{} + {} = {}".format(a, b, add(a, b)))
-    elif operator == "-":
-        print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif operator == "*":
-        print("{} * {} = {}".format(a, b, mul(a, b)))
-    else:
-        print("{} / {} = {}".format(a, b, div(a, b)))
+    print("{} {} {} = {}".format(a, argv[2], b, ops[argv[2]](a, b)))
