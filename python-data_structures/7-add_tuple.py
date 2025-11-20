acb7efe0825f5b4746d19464afa62545f26a8ff0
cayciahmet <nbiyevashla215@gmail.com>
2025-11-20 20:21:52 +0000

    Fix pycodestyle whitespace error

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    # Tuple-lara 0, 0 elave edirik ki, xeta vermesin
     tuple_a += (0, 0)
     tuple_b += (0, 0)
-    
-    # Ilk iki elementi toplayib qaytaririq
     return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
