92e24a151f29b4adc63fe9d0f45fa70db49201ce
cayciahmet <nbiyevashla215@gmail.com>
2025-11-21 13:02:30 +0000

    Fix pycodestyle errors

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string or type(roman_string) != str:
+    if not roman_string or not isinstance(roman_string, str):
         return 0
 
     rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
@@ -8,10 +8,10 @@ def roman_to_int(roman_string):
     length = len(roman_string)
 
     for i in range(length):
-        # Eger bu sonuncu herf deyilse ve bu herf sonrakindan kicikdirse
-        if i < length - 1 and rom_n[roman_string[i]] < rom_n[roman_string[i + 1]]:
-            result -= rom_n[roman_string[i]]
+        curr = rom_n[roman_string[i]]
+        if i < length - 1 and curr < rom_n[roman_string[i + 1]]:
+            result -= curr
         else:
-            result += rom_n[roman_string[i]]
+            result += curr
 
     return result
