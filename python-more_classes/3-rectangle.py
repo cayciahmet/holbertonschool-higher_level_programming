4b85ae1cca139a02d134cb8348ccde660374e2ff
cayciahmet <nbiyevashla215@gmail.com>
2025-11-25 05:34:43 +0000

    Fix pycodestyle whitespace error

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -44,7 +44,7 @@ class Rectangle:
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
             return ""
-        
+
         rect = []
         for i in range(self.__height):
             rect.append("#" * self.__width)
