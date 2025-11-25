feff9e982b53de0e50fdb0488e1db9f58ab41e21
cayciahmet <nbiyevashla215@gmail.com>
2025-11-25 07:44:39 +0000

    3. Same class or inherit from

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Same class or inherit from."""
+
+
+def is_kind_of_class(obj, a_class):
+    """Check if object is instance or inherited."""
+    return isinstance(obj, a_class)
