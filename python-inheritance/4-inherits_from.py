3de0f64a18672e1ac3d51d06e97009b57d4208d8
cayciahmet <nbiyevashla215@gmail.com>
2025-11-25 07:46:54 +0000

    Fix pycodestyle line length

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,5 +3,5 @@
 
 
 def inherits_from(obj, a_class):
-    """Check if object is an instance of a class that inherited from a_class."""
+    """Check if object inherits from a_class."""
     return isinstance(obj, a_class) and type(obj) is not a_class
