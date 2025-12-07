911bc5847d7d0aa4dc6334ccacd030174017dfa7
cayciahmet <nbiyevashla215@gmail.com>
2025-12-07 13:57:18 +0000

    9. My GitHub!

---
+++ python-network_1/10-my_github.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Takes your GitHub credentials (username and password) and uses the GitHub API to display your id"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    url = "https://api.github.com/user"
+
+    r = requests.get(url, auth=(username, password))
+    print(r.json().get("id"))
